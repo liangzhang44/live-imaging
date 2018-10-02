5f0e8845ff18dfd4eb86d0ac41b790b281f49d31
Liang Zhang <liazhang@uw.edu>
2018-10-02 15:59:24 -0700

    Remove green and yellow colors

--- figure 1.R
+++ figure 1.R
@@ -17,7 +17,7 @@ df <- data.frame(
 f1a <- ggplot(df, aes(xmin = left, xmax = right, ymin = down, ymax = up))+
     ylim(-8, 8)+
     theme_void()+
-    geom_rect(fill = c(rep("green", 7), rep("yellow", 20)))+
+    geom_rect(fill = "white", color = "black", size = 0.3)+
     annotate("segment", x = 14, xend = 46, y = 0, yend = 0)+
     annotate("segment", x = seq(55, length.out = 3, by = 21), 
              xend = seq(67, length.out = 3, by = 21), y = 0, yend = 0)+
@@ -29,9 +29,11 @@ f1a <- ggplot(df, aes(xmin = left, xmax = right, ymin = down, ymax = up))+
     annotate("text", x = 7.5, y = -2.5, label = "Training", size = 3)+
     annotate("text", x = seq(50.5, length.out = 4, by = 21), y = 2.5, 
              label = paste("Test", 1:4), size = 3)+
-    annotate("rect", xmin = c(0, 15, 37.5), xmax = c(97.5, 37.5, 82.5), ymin = 5.5, ymax = 7, 
-             fill = c("green", "#56B4E9", "orange"))+
+    annotate("rect", xmin = c(15, 37.5), xmax = c(37.5, 82.5), ymin = 5.5, ymax = 7, 
+             fill = c("#56B4E9", "orange"))+
     annotate("segment", x = 82.5, xend = 82.5, y = 5.5, yend = 7, color = "red")+
+    annotate("rect", xmin = 0, xmax = 97.5, ymin = 5.5, ymax = 7, 
+             color = "black", alpha = 0)+
     annotate("text", x = c(26, 60, 82.5), y = 7.5, label = c("Tone", "Trace", "Shock"), 
              color = c("#56B4E9", "orange", "red"), size = 3)+
     annotate("text", x = c(7.5, 26, 60, 90), y = 6.25, size = 3,
@@ -40,9 +42,12 @@ f1a <- ggplot(df, aes(xmin = left, xmax = right, ymin = down, ymax = up))+
     annotate("segment", x = 4, xend = 97.5, y = 2.5, yend = 5.5, lty = 2)+
     annotate("segment", x = 3, xend = 3, y = 2, yend = 2.5, lty = 1)+
     annotate("segment", x = 4, xend = 4, y = 2, yend = 2.5, lty = 1)+
-    annotate("rect", xmin = c(51.5, 66.5, 74), xmax = c(122, 74, 119), ymin = -7, ymax = -5.5, 
-             fill = c("yellow", "#56B4E9", "orange"))+
-    annotate("segment", x = 119, xend = 119, y = -5.5, yend = -7, lty = 6, size = 0.7, color = "red")+
+    annotate("rect", xmin = c(66.5, 74), xmax = c(74, 119), ymin = -7, ymax = -5.5, 
+             fill = c("#56B4E9", "orange"))+
+    annotate("segment", x = 119, xend = 119, y = -5.5, yend = -7, lty = 6, 
+             size = 0.7, color = "red")+
+    annotate("rect", xmin = 51.5, xmax = 122, ymin = -7, ymax = -5.5, 
+             color = "black", alpha = 0)+
     annotate("text", x = c(70.25, 96.5), y = -7.5, label = c("Tone", "Trace"),
              color = c("#56B4E9", "orange"), size = 3)+
     annotate("text", x = c(59, 70.25, 96.5), y = -6.25, size = 3, 
@@ -57,9 +62,6 @@ training <- read_xlsx("behavior scoring.xlsx", sheet = 1)
 training[,7:8] <- NULL
 training$Mean <- rowMeans(training[,-1])/10*100
 f1b <- ggplot(training, aes(Time, Mean))+
-    annotate("rect", xmin = seq(0, length.out = 7, by = 260), 
-             xmax = seq(65, length.out = 7, by = 260), 
-             ymin = -10, ymax = 100, fill = "green", alpha = 0.4)+
     annotate("rect", xmin = seq(10, length.out = 7, by = 260), 
              xmax = seq(25, length.out = 7, by = 260), 
              ymin = -10, ymax = 100, fill = "#56B4E9")+
@@ -69,12 +71,17 @@ f1b <- ggplot(training, aes(Time, Mean))+
     annotate("segment", x = seq(55, length.out = 7, by = 260), 
              xend = seq(55, length.out = 7, by = 260), 
              y = -10, yend = 100, color = "red")+
+    annotate("rect", xmin = seq(0, length.out = 7, by = 260), 
+             xmax = seq(65, length.out = 7, by = 260), ymin = -10, ymax = 100, 
+             color = "black", alpha = 0, size = 0.2)+
     geom_smooth(span = 0.1, se = TRUE)+
     geom_point(size = 0.5)+
-    annotate("rect", xmin = c(0, 280, 700), xmax = c(1820, 700, 1540), ymin = 130, ymax = 160, 
-             fill = c("green", "#56B4E9", "orange"))+
+    annotate("rect", xmin = c(280, 700), xmax = c(700, 1540), ymin = 130, ymax = 160, 
+             fill = c("#56B4E9", "orange"))+
     annotate("segment", x = 1540, xend = 1540, y = 130, yend = 160, color = "red")+
-    annotate("text", x = c(490, 1120, 1540), y = 168, label = c("Tone", "Trace", "Shock"),
+    annotate("rect", xmin = 0, xmax = 1820, ymin = 130, ymax = 160, 
+             color = "black", alpha = 0)+
+    annotate("text", x = c(490, 1120, 1540), y = 170, label = c("Tone", "Trace", "Shock"),
              color = c("#56B4E9", "orange", "red"), size = 3)+
     annotate("text", x = c(140, 490, 1120, 1680), y = 145, size = 3, 
              label = paste(c(10, 15, 30, 9), "sec"))+
@@ -82,7 +89,7 @@ f1b <- ggplot(training, aes(Time, Mean))+
     annotate("segment", x = 260+65, xend = 1820, y = 105, yend = 130, lty = 2)+
     annotate("segment", x = 260, xend = 260, y = 100, yend = 105, lty = 1)+
     annotate("segment", x = 260+65, xend = 260+65, y = 100, yend = 105, lty = 1)+
-    annotate("text", x = seq(30, length.out = 7, by = 260), y = -16, 
+    annotate("text", x = seq(30, length.out = 7, by = 260), y = -18, 
              label = 1:7, size = 3)+
     annotate("text", x = 1780, y = -16, label = "(Session)", size = 3)+
     scale_y_continuous(breaks = c(0, 50, 100, 150), labels = c("0", "50", "100", ""))+
@@ -102,12 +109,9 @@ for (i in 1:4) {
 colnames(test) <- c("Time", "Test 1", "Test 2", "Test 3", "Test 4")
 test <- gather(test, key = "Test", value = "Freezing", -Time)
 
-cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442")
+cbPalette <- c("#0072B2", "#CC79A7", "#009E73", "#F0E442")
 f1c <- ggplot(test, aes(Time, Freezing, color = Test))+
     scale_colour_manual(values = cbPalette)+
-    annotate("rect", xmin = seq(0, length.out = 5, by = 170), 
-             xmax = seq(47, length.out = 5, by = 170), 
-             ymin = -10, ymax = 50, fill = "yellow")+
     annotate("rect", xmin = seq(10, length.out = 5, by = 170), 
              xmax = seq(15, length.out = 5, by = 170), 
              ymin = -10, ymax = 50, fill = "#56B4E9")+
@@ -117,11 +121,17 @@ f1c <- ggplot(test, aes(Time, Freezing, color = Test))+
     annotate("segment", x = seq(45, length.out = 5, by = 170), 
              xend = seq(45, length.out = 5, by = 170), 
              y = -10, yend = 50, color = "red", lty = 6)+
+    annotate("rect", xmin = seq(0, length.out = 5, by = 170), 
+             xmax = seq(47, length.out = 5, by = 170), ymin = -10, ymax = 50, 
+             color = "black", alpha = 0, size = 0.2)+
     geom_smooth(aes(group = Test), span = 0.1, size = 0.5)+
     geom_point(size = 0.5)+
-    annotate("rect", xmin = c(0, 181, 271), xmax = c(850, 271, 814), ymin = 70, ymax = 84, 
-             fill = c("yellow", "#56B4E9", "orange"))+
-    annotate("segment", x = 814, xend = 814, y = 70, yend = 84, lty = 6, size = 0.7, color = "red")+
+    annotate("rect", xmin = c(181, 271), xmax = c(271, 814), ymin = 70, ymax = 84, 
+             fill = c("#56B4E9", "orange"))+
+    annotate("segment", x = 814, xend = 814, y = 70, yend = 84, lty = 6, 
+             size = 0.7, color = "red")+
+    annotate("rect", xmin = 0, xmax = 850, ymin = 70, ymax = 84,
+             color = "black", alpha = 0)+
     annotate("text", x = c(226, 543), y = 90, label = c("Tone", "Trace"),
              color = c("#56B4E9", "orange"), size = 3)+
     annotate("text", x = c(90, 226, 543), y = 77, size = 3, 
@@ -143,4 +153,4 @@ f1c <- ggplot(test, aes(Time, Freezing, color = Test))+
 figure1 <- ggarrange(f1a, f1b, f1c, labels = c("A", "B", "C"), nrow = 3, ncol = 1)
 figure1 <- annotate_figure(figure1, fig.lab = "Figure 1", fig.lab.face = "bold",
                            fig.lab.size = 14, top = text_grob(""))
-ggsave(figure1, filename = "figure 1.pdf", height = 16, width = 11.6, units = "cm")
+ggsave(figure1, filename = "figure 1.pdf", height = 17, width = 11.6, units = "cm")
